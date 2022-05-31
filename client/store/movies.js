66635fd73ff53651f04bb8435cdbb08d7f0426c2
Jiayu Zhang <jiayuzhangg@gmail.com>
2022-05-30 21:19:38 -0400

    moved request to backend

--- client/store/movies.js
+++ client/store/movies.js
@@ -18,7 +18,7 @@ const SET_MOVIES = 'SET_MOVIES'
  */
 export const loadMovies = (page=1)=>{
   return async(dispatch) => {
-    const movies = (await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_KEY }&page=${page}`)).data
+    const movies = (await axios.get(`/api/movies/${page}`)).data
     dispatch({
       type:SET_MOVIES,
       movies
--- server/api/movies.js
+++ server/api/movies.js
@@ -1,3 +1,21 @@
 const router = require('express').Router()
 module.exports = router
+const axios = require('axios')
+require('dotenv').config();
 
+router.get('/', async(req, res, next)=>{
+  try{
+    res.json('hi')
+  }catch(err){
+    next(err)
+  }
+})
+
+router.get('/:page', async(req, res, next)=>{
+  try{
+    const movies = (await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_MOVIE_KEY }&page=${req.params.page}`)).data
+    res.json(movies)
+  }catch(err){
+    next(err)
+  }
+})
