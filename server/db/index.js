b8a7f5b31d940d9714922f82ceff5a0ba2966e97
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2022-05-31 17:30:55 -0400

    added model associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,27 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Comments = require("./models/Comments");
+const Friends = require("./models/Friends");
+const Media = require("./models/Media");
+const Ratings = require("./models/Ratings");
 
 //associations could go here!
+User.hasMany(Comments);
+User.hasMany(Ratings);
+Media.hasMany(Ratings);
+User.hasMany(Friends);
+Ratings.hasMany(Comments);
 
 module.exports = {
   db,
   models: {
     User,
+    Comments,
+    Friends,
+    Media,
+    Ratings,
   },
-}
+};
