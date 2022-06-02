897a98e1ebec838dcd57e3208d3b1dfe1abca9b1
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2022-06-02 19:51:59 -0400

    working on profile, friends button

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,29 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
+import { addFriend } from "../store/relationships";
 
-const Profile = (state) => {
-  console.log("state", state);
-  const { auth, relationships } = state;
+const Profile = (props) => {
+  console.log("state", props.state);
+  const { auth, relationships } = props.state;
 
-  const filteredRelationship = relationships.filter((rel) => {
-    return rel.senderId === auth.id;
-  });
-  console.log(filteredRelationship);
-  const friendsNumber = 0;
-  filteredRelationship.forEach((request) => {
-    return request.status === "accepted" ? friendsNumber + 1 : null;
-  });
-  console.log(friendsNumber);
+  const acceptedFriends = relationships
+    .filter((rel) => {
+      return rel.senderId === auth.id;
+    })
+    .filter((request) => request.status === "accepted");
+
   return (
     <div>
       <div>Wallpaper</div>
       <div>Profile Pic</div>
       <div>{auth.username}</div>
       <div>Bio, we can add this as part of the User db</div>
-      <div>{friendsNumber} Friends</div>
-      <button onClick={() => console.log("add friend")}>Add Friend</button>
+      <div> {acceptedFriends.length} Friends</div>
+      {/* need to change page to be the users id, based on who is logged in */}
+      {/* <button onClick={() => props.addFriend(user.id, auth.id)}>
+        Add Friend
+      </button> */}
     </div>
   );
 };
-
-export default connect((state) => state)(Profile);
+const mapDispatch = (dispatch) => {
+  return {
+    addFriend: async (senderId, recipientId) => {
+      await dispatch(addFriend(senderId, recipientId));
+    },
+  };
+};
+export default connect((state) => state, mapDispatch)(Profile);
--- client/store/relationships.js
+++ client/store/relationships.js
@@ -1,42 +1,56 @@
-import axios from 'axios'
-import history from '../history'
+import axios from "axios";
+import history from "../history";
 
-const TOKEN = 'token'
+const TOKEN = "token";
 
 /**
  * ACTION TYPES
  */
-const SET_RELATIONSHIPS = 'SET_RELATIONSHIPS'
+const SET_RELATIONSHIPS = "SET_RELATIONSHIPS";
+const ADD_FRIEND = "ADD_FRIEND";
 
 /**
  * ACTION CREATORS
  */
 
-
 /**
  * THUNK CREATORS
  */
-export const loadRelationships = ()=>{
+export const loadRelationships = () => {
   // const user = store.getState()
   // console.log(user)
-  return async(dispatch) => {
-    const relationships = (await axios.get(`/api/relationships`)).data
+  return async (dispatch) => {
+    const relationships = (await axios.get(`/api/relationships`)).data;
     dispatch({
-      type:SET_RELATIONSHIPS,
-      relationships
-    })
-  }
-}
-
+      type: SET_RELATIONSHIPS,
+      relationships,
+    });
+  };
+};
+export const addFriend = (senderId, recipientId) => {
+  return async (dispatch) => {
+    const requestResponse = await axios.post(`/api/relationships/addfriend`, {
+      senderId,
+      recipientId,
+    });
+    const request = requestResponse.data;
+    dispatch({
+      type: ADD_FRIEND,
+      request,
+    });
+  };
+};
 
 /**
  * REDUCER
  */
-export default function(state = [], action) {
+export default function (state = [], action) {
   switch (action.type) {
     case SET_RELATIONSHIPS:
-      return action.relationships
+      return action.relationships;
+    case ADD_FRIEND:
+      return action.request;
     default:
-      return state
+      return state;
   }
 }
