d22a02e273660795df87a3573a70c31881aa463f
Jiayu Zhang <jiayuzhangg@gmail.com>
2022-06-06 00:14:37 -0400

    added more search functionality

--- client/components/Filters.js
+++ client/components/Filters.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef} from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
-import {Button, InputLabel, MenuItem, FormControl, Select, Chip} from '@material-ui/core'
+import {Button, InputLabel, MenuItem, FormControl, Select, Chip, TextField} from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
@@ -25,7 +25,12 @@ const useStyles = makeStyles((theme) => ({
   button:{
     display:'flex',
     alignItems:'center'
-  }
+  }, root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
 }));
 
 const ITEM_HEIGHT = 48;
@@ -60,12 +65,14 @@ export default function MultipleSelect(props) {
   const classes = useStyles();
   const theme = useTheme();
   const [genre, setGenre] = React.useState([]);
-  const {genres, media} = props
+  const {genres, media, nameSearch, peopleSearch} = props
   const prevMedia = usePrevious(media)
+  const prevNameSearch = usePrevious(nameSearch)
+  const prevPeopleSearch = usePrevious(peopleSearch)
   const selectedGenres = genres
 
   useEffect(() => {
-    if(prevMedia !== media){
+    if(prevMedia !== media || prevNameSearch !== nameSearch || prevPeopleSearch !== peopleSearch){
       props.setNewGenres()
       setGenre([])
     }
@@ -141,5 +148,38 @@ export function BasicSelects(props) {
         </Select>
       </FormControl>
     </div>
+  );
+}
+
+export function SearchBox(props) {
+  const classes = useStyles();
+  const {genres, onChangeValue, searchBy, nameSearch, peopleSearch, media} = props
+  const prevGenres = usePrevious(genres)
+  const prevNameSearch = usePrevious(nameSearch)
+  const prevPeopleSearch = usePrevious(peopleSearch)
+  const prevMedia = usePrevious(media)
+  const [name, setName] = React.useState('');
+  const [people, setPeople] = React.useState('');
+
+  useEffect(() => {
+    if(searchBy ==='title' && (prevGenres?.length !== genres?.length || prevPeopleSearch !== peopleSearch)){
+      onChangeValue('')
+      setName('')
+    }
+    if(searchBy ==='people' && (prevGenres?.length !== genres?.length || prevNameSearch !== nameSearch)){
+      onChangeValue('')
+      setPeople('')
+    }
+  })
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField id="standard-basic" value={searchBy==='title'?name:people} label={`Search by ${searchBy}`}
+        onChange={(ev)=> {
+          searchBy === 'title'?setName(ev.target.value):setPeople(ev.target.value)
+          onChangeValue(ev.target.value)
+        }}/>
+      <Button className={classes.button} onClick={()=>{searchBy==='title'?setName(''):setPeople(''), onChangeValue('')}}>Reset</Button>
+    </form>
   );
 }
