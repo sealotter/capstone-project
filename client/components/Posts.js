92a93250ba86156bc56c32a0511f5aa87834d231
Angel Acosta <angel.s.acosta1@gmail.com>
2022-06-08 20:59:24 -0400

    more post styling, including date created

--- client/components/Posts.js
+++ client/components/Posts.js
@@ -7,26 +7,34 @@ import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 const Posts = ({ posts }) => {
+  console.log(posts);
   return (
     <div>
       {posts.map((post) => {
         return (
-          <div key={post.id} className="post">
-            <div className="post_body">
-              <Avatar src={post.avatarUrl} />
-              <div className="post_header">
-                <div className="post_headerText">
-                  <h3>{post.username} </h3>
-                </div>
-                <div className="post_headerDesc">
-                  <p>{post.content}</p>
+          <div key={post.id} className="postOut">
+            <div className="post">
+              <div className="post_body">
+                <Avatar src={post.avatarUrl} />
+                <div className="post_header">
+                  <div className="post_headerText">
+                    <h3>
+                      {post.username}{' '}
+                      <span className="postDetail">
+                        •{Date(post.createdAt).slice(4, 10)}{' '}
+                      </span>
+                    </h3>
+                  </div>
+                  <div className="post_headerDesc">
+                    <p>{post.content}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="post_footer">
-              <ChatBubbleOutlineIcon fontSize="small" />
+              <div className="post_footer">
+                <ChatBubbleOutlineIcon fontSize="small" />
 
-              <FavoriteBorderIcon fontSize="small" />
+                <FavoriteBorderIcon fontSize="small" />
+              </div>
             </div>
           </div>
         );
