07b35779ba152f9e448a9fc4dde417e167b46516
Jiayu Zhang <jiayuzhangg@gmail.com>
2022-06-20 20:49:56 -0400

    commit

--- client/components/SingleMedia.js
+++ client/components/SingleMedia.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { findSingleMedia, createList, createPost, updatePostContent } from '../store';
-import { Link } from 'react-router-dom';
 import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import Recommendations from './Recommendations';
 import MultilineTextFields from './Textfield'
 import { Button } from '@material-ui/core';
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 class SingleMedia extends React.Component {
@@ -90,81 +93,107 @@ class SingleMedia extends React.Component {
     };
 
     return (
-      <div>
-        <img
-          src={
-            media.poster_path
-              ? `https://image.tmdb.org/t/p/w300/${media.poster_path}`
-              : 'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png'
-          }
-        />
-        <h1>{media.title}</h1>
-        <p>Genres: {combineArr(media.genres)}</p>
-        <p>Runtime: {media.runtime} minutes</p>
-        <p>Critic Rating: {media.vote_average}/10</p>
-        <p>
-          User Rating:{' '}
-          {media.dataValues.numOfRatings === 0
-            ? 'Not rated yet!'
-            : `${(
-                media.dataValues.totalRating / media.dataValues.numOfRatings
-              ).toFixed(1)}/10`}
-        </p>
-        <p>Release date: {media.release_date}</p>
-        <p>Budget: ${media.budget}</p>
-        <p>Revenue: ${media.revenue}</p>
-        <p>Produced by: {combineArr(media.production_companies)}</p>
-        <p>
-          Website:{' '}
-          {media.homepage ? (
-            <a href={`${media.homepage}`}>{media.homepage}</a>
-          ) : (
-            'N/A'
-          )}
-        </p>
-        <p>Overview: {media.overview}</p>
-        <div className='watchBtn'>
-            <Button onClick={() => this.handleOnClick(media)}>Add to Watch List</Button>
+      <>
+        <h2 className='singlemediatitle'>{media.title}</h2>
+        <div className='singlemediacontainer'>
+          <div className='singlemediaimage'>
+            <img className='singlemediaposter'
+              src={
+                media.poster_path
+                  ? `https://image.tmdb.org/t/p/w300/${media.poster_path}`
+                  : 'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png'
+              }
+            />
+            <div className='viewrating'>
+              <h2>User ratings</h2>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>Show what others thought!</Typography>
+                </AccordionSummary>
+                  {ratings.length
+                    ? ratings.map((item) => {
+                        return (
+                          <AccordionDetails key={item.id}>
+                            <Typography>
+                              {users.find((user) => user.id === item.userId).username}{' '}
+                              <Rating
+                                name="Rated"
+                                readOnly
+                                value={item.rating ? item.rating : 0}
+                                max={10}
+                              />
+                              {item.content?<p>{item.content}</p>:null}
+                            </Typography>
+                          </AccordionDetails>
+                        );
+                      })
+                    : 'No ratings yet!'}
+              </Accordion>
+            </div>
+          </div>
+          <div className='singlemediacontent'>
+            {/* <p>Genres: {combineArr(media.genres)}</p> */}
+            <div className='genres'>
+              <div><span className='category'>Genres: </span></div>
+              <ul className='genrecontent'>
+                {media.genres.map(genre=><li key={genre.id} className='singlegenre'>{genre.name}</li>)}
+              </ul>
+            </div>
+            <p><span className='category'>Overview: </span>{media.overview}</p>
+            <p><span className='category'>Runtime: </span> {media.runtime} Minutes</p>
+            <p><span className='category'>Critic Rating: </span>{media.vote_average}/10</p>
+            <p>
+              <span className='category'>User Rating: </span>{' '}
+              {media.dataValues.numOfRatings === 0
+                ? 'Not rated yet!'
+                : `${(
+                    media.dataValues.totalRating / media.dataValues.numOfRatings
+                  ).toFixed(1)}/10`}
+            </p>
+            <p><span className='category'>Release date: </span>{media.release_date}</p>
+            <p><span className='category'>Produced by: </span>{combineArr(media.production_companies)}</p>
+            <p id='website'>
+              <span className='category'>Website: </span>{' '}
+              {media.homepage ? (
+                <a href={`${media.homepage}`}>{media.homepage}</a>
+              ) : (
+                'N/A'
+              )}
+            </p>
+            <div className='selectContainer'>
+              <div className='watchBtn'>
+                <Button onClick={() => this.handleOnClick(media)}>Add to Watch List</Button>
+              </div>
+              <div className='makerecommendation'>
+                <Recommendations media={media} />
+              </div>
+            </div>
+            <div className='makeratingcontainer'>
+              <div className='makerating'>
+                <div>
+                  <Typography component="legend">
+                    <h2>Rate this movie!</h2>
+                  </Typography>
+                  <Rating
+                    name="Rating"
+                    value={myRating?.rating ? myRating.rating : 0}
+                    max={10}
+                    onChange={(ev) => {
+                      createPost(null, auth.id, null, media.id, ev.target.value * 1);
+                      findSingleMedia({id, media:type})
+                    }}
+                  />
+                </div>
+                {myRating?<MultilineTextFields handleSubmit={updatePostContent} postId={myRating.id}/>:null}
+              </div>
+            </div>
+          </div>
         </div>
-
-        <Box component="fieldset" mb={3} borderColor="transparent">
-          <Typography component="legend">
-            <h2>Rate this movie!</h2>
-          </Typography>
-          <Rating
-            name="Rating"
-            value={myRating?.rating ? myRating.rating : 0}
-            max={10}
-            onChange={(ev) => {
-              createPost(null, auth.id, null, media.id, ev.target.value * 1);
-              findSingleMedia({id, media:type})
-            }}
-          />
-        </Box>
-        {myRating?<>
-          <MultilineTextFields handleSubmit={updatePostContent} postId={myRating.id}/>
-        </>:null}
-        <ul>
-          <h2>User ratings</h2>
-          {ratings.length
-            ? ratings.map((item) => {
-                return (
-                  <li key={item.id}>
-                    {users.find((user) => user.id === item.userId).username}{' '}
-                    <Rating
-                      name="Rated"
-                      readOnly
-                      value={item.rating ? item.rating : 0}
-                      max={10}
-                    />
-                  </li>
-                );
-              })
-            : 'No ratings yet!'}
-        </ul>
-
-        <Recommendations media={media} />
-      </div>
+      </>
     );
   }
 }
